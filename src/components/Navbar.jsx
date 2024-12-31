e0f6ca8a13b85a39a2da9c966dbb173c1032227f
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2024-12-31 12:14:16 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React, { useRef, useState } from 'react';
+import "../App.css"
+import { gsap } from 'gsap';
+function Navbar() {
+
+const [isVisible , setIsVisible ] = useState(false);
+const hamburger = useRef(null)
+const handleClick = () =>{
+    setIsVisible(!isVisible)
+
+    hamburger.current.classList.toggle('open');
+}
+
+const transformValue = isVisible ? "0px" : '-1000px';
+
+gsap.to('.main' , {
+    duration : 0.3 ,
+    transform  : `translateY(${transformValue})`
+})
+    return (
+<>
+<div className="main" >
+            <div className="logo">
+                <h1 className="text-4xl font-bold p-4">ochi</h1>
+                <hr className="border-gray-400" />
+            </div>
+
+            <nav>
+                <ul className="navbar">
+                    <li >Service</li>
+                    <li >Our work</li>
+                    <li >About</li>
+                    <li >Insight</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+
+            <div className="socials mt-4">
+                <ul className="flex space-x-6">
+                    <li >LinkedIn</li>
+                    <li >Github</li>
+                    <li >Discord</li>
+                </ul>
+            </div>
+
+            <div className="adress mt-4">
+                <ul>
+                    <li>202-1965 W 4th Ave <br />
+                        Vancouver, Canada
+                    </li>
+                    <li>30 Chukarina St <br />
+                        Lviv, Ukraine
+                    </li>
+                </ul>
+            </div>
+
+            <div className="email mt-4">
+                <ul>
+                    <li>hello@ochi.design</li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="nav">
+        <div className="logos">
+                <h1 >ochi</h1>
+            </div>
+
+            <nav>
+                <ul className="uls">
+                    <li >Service</li>
+                    <li >Our work</li>
+                    <li >About</li>
+                    <li >Insight</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+            <div className="hamburger" ref={hamburger} onClick={handleClick}>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+</>
+    )
+}
+
+export default Navbar;
